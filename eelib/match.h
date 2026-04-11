32719c7f40f31f72e3f7174a370dce41c2776202
Jacob Stahl <50686850+Jacob-Stahl@users.noreply.github.com>
2026-04-10 22:03:00 -0500

    cleaned up Match

--- eelib/match.h
+++ eelib/match.h
@@ -11,18 +11,18 @@ struct Match{
     long qty;
 
     public:
-        Match(const Order& ord1, const Order& ord2, long qty)
+        Match(const Order& ord1, const Order& ord2, long qty_)
         {
             if (ord1.side == ord2.side) { std::logic_error("Can't match orders on the same side!"); }
             if (ord1.side == BUY){
-                this->buyer = ord1;
-                this->seller = ord2;
+                buyer = ord1;
+                seller = ord2;
             }
             else{
-                this->buyer = ord2;
-                this->seller = ord1;
+                buyer = ord2;
+                seller = ord1;
             }
 
-            this->qty = qty;
+            qty = qty_;
         }
 };
